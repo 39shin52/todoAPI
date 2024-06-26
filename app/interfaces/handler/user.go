092261395b063e282be8f849195e23cecceabd6b
39shin52 <g2122006@fun.ac.jp>
2024-06-26 23:25:38 +0900

    [add] create userHandler

--- app/interfaces/handler/user.go
+++ app/interfaces/handler/user.go
@@ -1,3 +1,47 @@
 package handler
 
+import (
+	"net/http"
+
+	"github.com/39shin52/todoAPI/app/domain/entity"
+	"github.com/39shin52/todoAPI/app/usecase"
+	"github.com/gin-gonic/gin"
+)
+
 // gin使う予定
+
+type UserHandler struct {
+	userUsecase *usecase.UserUsecase
+}
+
+func NewUserHandler(userUsecase *usecase.UserUsecase) *UserHandler {
+	return &UserHandler{userUsecase: userUsecase}
+}
+
+func (uh UserHandler) GetUser(c *gin.Context) {
+	var userName string
+	if err := c.Bind(&userName); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+		})
+
+		return
+	}
+
+	user, err := uh.userUsecase.SelectUser(userName)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+
+		return
+	}
+
+	userResponse := entity.User{
+		ID:   user.ID,
+		Mail: user.Mail,
+		Work: user.Work,
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"response": userResponse,
+	})
+}
